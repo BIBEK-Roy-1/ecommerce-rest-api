f6eb31701afd8647d0af6b2b1627d91c33c3e708
Bibek Roy <bibekroy876@gmail.com>
2026-07-14 00:47:23 +0530

    Fix deployed Swagger documentation route

--- server.js
+++ server.js
@@ -1,42 +1,36 @@
 import "dotenv/config";
 import express from "express";
-import swagger from "swagger-ui-express";
+import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" with { type: "json" };
+
 import ProductRouter from "./src/features/product/Product.routes.js";
-import bodyParser from "body-parser";
 import userrouter from "./src/features/user/user.route.js";
-import basicAuthorizer from "./src/middlewares/basicauth.middleware.js";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
 import cartrouter from "./src/features/cart/cartitems.routes.js";
-import apiDocs from './swagger.json' with { type: 'json' };
 
+const server = express();
 
-const server=express();
+server.use(express.json());
 
+server.get("/", (req, res) => {
+  res.send("Welcome to Ecommerce APIs");
+});
 
+// Redirect to the trailing-slash URL
+server.get("/api-docs", (req, res) => {
+  res.redirect("/api-docs/");
+});
 
-
-
-server.get("/",(req,res)=>{
-    res.send("welcome to Ecommerce APIs")
-})
-server.use(bodyParser.json());
-
-
-//for all the rquest related to product ,redirect to product routes.
-// server.use("/api/products",basicAuthorizer,ProductRouter);
-server.use("/api/products",jwtAuth,ProductRouter);
-server.use("/api/user",userrouter);
-server.use("/api/cartItems",jwtAuth,cartrouter);
-
-server.use('/api-docs',swagger.serve,swagger.setup(apiDocs));
-
+// Swagger documentation
 server.use(
-  "/api-docs",
-  swagger.serve,
-  swagger.setup(swaggerDocument)
+  "/api-docs/",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
 );
 
+server.use("/api/products", jwtAuth, ProductRouter);
+server.use("/api/user", userrouter);
+server.use("/api/cartItems", jwtAuth, cartrouter);
 
 const PORT = process.env.PORT || 3100;
 
